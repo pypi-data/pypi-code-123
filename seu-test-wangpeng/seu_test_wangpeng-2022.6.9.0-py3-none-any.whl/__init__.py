e0d872c83724e13011d7aa11785fb88d59b1027b
Tom Forbes <tom@tomforb.es>
2022-06-09 03:23:55 +0000

    {"name":"seu-test-wangpeng","version":"2022.6.9.0","file":"seu_test_wangpeng-2022.6.9.0-py3-none-any.whl","path":"seu-test-wangpeng/seu_test_wangpeng-2022.6.9.0-py3-none-any.whl"}

---
+++ seu-test-wangpeng/seu_test_wangpeng-2022.6.9.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""seu-test-wangpeng
+https://github.com/apachecn/seu-test-wangpeng"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.6.9.0"
