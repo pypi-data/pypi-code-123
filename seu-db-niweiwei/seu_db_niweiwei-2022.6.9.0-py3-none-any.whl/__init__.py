1f06de4b3bb8472fb3acc5e3d08538ff6a9eb199
Tom Forbes <tom@tomforb.es>
2022-06-09 02:50:27 +0000

    {"name":"seu-db-niweiwei","version":"2022.6.9.0","file":"seu_db_niweiwei-2022.6.9.0-py3-none-any.whl","path":"seu-db-niweiwei/seu_db_niweiwei-2022.6.9.0-py3-none-any.whl"}

---
+++ seu-db-niweiwei/seu_db_niweiwei-2022.6.9.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""seu-db-niweiwei
+https://github.com/apachecn/seu-db-niweiwei"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.6.9.0"
