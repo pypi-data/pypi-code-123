c5745610625cf8fc1873d117e3a40a8610156d7a
Tom Forbes <tom@tomforb.es>
2022-06-09 03:14:02 +0000

    {"name":"seu-net-chengguang","version":"2022.6.9.0","file":"seu-net-chengguang-2022.6.9.0.tar.gz","path":"seu-net-chengguang/seu-net-chengguang-2022.6.9.0.tar.gz"}

---
+++ seu-net-chengguang/seu-net-chengguang-2022.6.9.0.tar.gz/SeuNetChengguang/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""seu-net-chengguang
+https://github.com/apachecn/seu-net-chengguang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.6.9.0"
