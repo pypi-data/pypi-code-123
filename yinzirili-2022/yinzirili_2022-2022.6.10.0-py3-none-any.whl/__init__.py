3827b60dc494b03320a4e9491452750458687230
Tom Forbes <tom@tomforb.es>
2022-06-10 03:47:44 +0000

    {"name":"yinzirili-2022","version":"2022.6.10.0","file":"yinzirili_2022-2022.6.10.0-py3-none-any.whl","path":"yinzirili-2022/yinzirili_2022-2022.6.10.0-py3-none-any.whl"}

---
+++ yinzirili-2022/yinzirili_2022-2022.6.10.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yinzirili-2022
+https://github.com/apachecn/yinzirili-2022"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.6.10.0"
